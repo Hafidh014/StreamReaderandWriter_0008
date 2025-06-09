341e46608571184d519cfebd7854d1af72420e3d
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-09 10:47:26 +0700

    main function selesai

--- expetionStandart.cpp
+++ expetionStandart.cpp
@@ -6,5 +6,15 @@ using namespace std;
 
 int main()
 {
+    cout << "awal program" <<endl;
+    try{
+        array <int, 3> data = {1, 2, 3, };
+        cout << data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        cout << e.what() <<endl;
+    }
+    cout << "baris program yang terahir " <<endl;
     
+    return 0;
 }
