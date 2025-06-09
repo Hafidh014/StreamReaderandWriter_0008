0cc14a68d860cc8b8b69035966cd459b84a7b0de
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-09 10:04:32 +0700

    memasukan nilai

--- iofile.cpp
+++ iofile.cpp
@@ -17,6 +17,14 @@ int mai(){
     //unlimited loop untuk menulis
     while (true) {
         cout << "- ";
-        
+
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+
+        //loop akan berhenti jika anda masukan karakter q
+        if (baris == "q") break;
+
+        oufile << baris << endl;
+
     }
 }
