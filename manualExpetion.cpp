7754b55d52941e8e4fb4358dc80d3242cd4a7279
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-09 10:38:00 +0700

    manual expetion

--- manualExpetion.cpp
+++ manualExpetion.cpp
@@ -6,6 +6,15 @@ int main()
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         throw 0.5;
-        
+        cout << "pernyataan tidak akan dieksekusi" <<endl;
     }
+    catch (int a) {
+        //blok ini akan di eksekusi
+        cout << "pengecualian akan di esekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer mala block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" <<endl;
+    }
+    return 0;
 }
