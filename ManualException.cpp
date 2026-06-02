ee9e822b85706661a7201173021a132cc77cc544
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-06-02 15:52:19 +0700

    untuk pengujian error

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+}
