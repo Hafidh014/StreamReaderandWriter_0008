c1dd047b3d896fd5895a272200376ba3b1e13475
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-06-02 16:05:45 +0700

    constructor

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -4,3 +4,17 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
+class TokoElektronik {
+private:
+    array<string, 3> etalase;
+
+public:
+    TokoElektronik() {
+        etalase[0] = "Laptop ASUS";
+        etalase[1] = "Smartphone Samsung";
+        etalase[2] = "Printer Epson";
+    }
+
+};
